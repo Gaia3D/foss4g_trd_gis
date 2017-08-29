0b162f6ff9df740c08bdba6e8d0f433ffbed76cb
BJ Jang <jangbi882@gmail.com>
2017-08-29 12:23:56 +0900

    initial

---
+++ 3ndGen_AdvSql.sql
@@ -0,0 +1,7 @@
+select * from subway_station
+where id in
+(
+	select distinct st.id
+	from subway_station as st, road_link2 as road
+	where road.lanes >= 8 and ST_Distance(st.geom, road.geom) <= 500
+)
