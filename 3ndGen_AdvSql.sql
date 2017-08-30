7b09786a179cef9264048ec0e9cb03d1c67d00ad
BJ Jang <jangbi882@gmail.com>
2017-08-30 10:55:59 +0900

    SQL 보기 쉽게 정리

--- 3ndGen_AdvSql.sql
+++ 3ndGen_AdvSql.sql
@@ -3,5 +3,6 @@ where id in
 (
 	select distinct st.id
 	from subway_station as st, road_link2 as road
-	where road.lanes >= 8 and ST_Distance(st.geom, road.geom) <= 500
+	where road.lanes >= 8
+	  and ST_Distance(st.geom, road.geom) <= 500
 )
